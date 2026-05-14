0f2bb778de21c1bfeb7b635c94b05d78b236c508
Avagatech <avagatech@gmail.com>
2026-05-14 17:19:25 +0300

    Update config.js

--- config.js
+++ config.js
@@ -163,7 +163,7 @@ const sequelize = (() => {
   });
 })();
 
-const SESSION_STRING = process.env.SESSION || process.env.SESSION_ID;
+const SESSION_STRING = process.env.SESSION || process.env.SESSION_ID;"RGNK~hD0wfRaJ"
 
 const SESSION = SESSION_STRING
   ? SESSION_STRING.split(",").map((s) => s.split("~")[1].trim())
